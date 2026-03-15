cd8f0c278fe860d340c62f69243133bdc1dfd951
13imaya2005 <13imaya2005@gmail.com>
2026-03-15 22:59:15 +0530

    first commit

---
+++ InvoiceCoreApi/Program.cs
@@ -0,0 +1,38 @@
+using InvoiceAPI.Services;
+using InvoiceCoreAPI.Repositories;
+using InvoiceCoreAPI.Contracts;
+using InvoiceCoreAPI.Data;
+using InvoiceCoreAPI.Mapper;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+builder.Services.AddScoped<IItemmasterRepository, ItemmasterRepository>();
+builder.Services.AddScoped<IItemMasterService, ItemMasterService>();
+builder.Services.AddAutoMapper(typeof(ItemMasterProfile));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
