73e9a2dd05869844afa77b8dbb864c0bdbadc2ea
13imaya2005 <13imaya2005@gmail.com>
2026-08-02 10:59:00 +0530

    users testing

--- InvoiceCoreApi/obj/Debug/net8.0/InvoiceCoreApi.AssemblyInfo.cs
+++ InvoiceCoreApi/obj/Debug/net8.0/InvoiceCoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceCoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8351a59cda1930fa7ab3f8b8fdb35941d6459e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409287a37d6f31780d1c17fecfc998d2df09019a")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceCoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceCoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
